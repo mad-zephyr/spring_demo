8311b05c7144df4e79871bb8ec4d8d16f011aaf7
Vasilii Iatiuc <vasilii.iatiuc@endava.com>
2023-04-20 21:47:55 +0300

    Bean lifecucle

--- src/main/java/org/spring/demo1/AppSpring.java
+++ src/main/java/org/spring/demo1/AppSpring.java
@@ -8,20 +8,9 @@ public class AppSpring {
                 "applicationContext.xml"
         );
 
-//        Dependency control trough
-//        Music music = context.getBean("music", Music.class);
-//        MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();
 
-        TestBean testBean = context.getBean("testBean", TestBean.class);
-
-        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
-
-        System.out.println(musicPlayer1 == musicPlayer2);
-        System.out.println(musicPlayer1);
-        musicPlayer1.playMusic();
-
-        System.out.println(testBean.getName());
         context.close();
     }
 }
--- src/main/java/org/spring/demo1/ClassicalMusic.java
+++ src/main/java/org/spring/demo1/ClassicalMusic.java
@@ -1,6 +1,13 @@
 package org.spring.demo1;
 
-public class ClassicalMusic implements Music{
+public class ClassicalMusic implements Music {
+    public void init() {
+        System.out.println("Doing bean initialization");
+    }
+    public void destroy() {
+        System.out.println("Classic Bean destroying, GoodBye");
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
