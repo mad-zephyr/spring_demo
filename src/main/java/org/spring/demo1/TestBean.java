ebfec06ee8b84a88fe2d40f9e596d895967f217b
Vasilii Iatiuc <vasilii.iatiuc@endava.com>
2023-04-20 16:05:53 +0300

    Spring project initializing

---
+++ src/main/java/org/spring/demo1/TestBean.java
@@ -0,0 +1,17 @@
+package org.spring.demo1;
+
+public class TestBean {
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
